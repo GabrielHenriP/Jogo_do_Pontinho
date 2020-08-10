2c80e7c7e3fb657c831a924fff33b0385303dc70
soadhenri <62116988+soadhenri@users.noreply.github.com>
2020-08-10 20:53:47 -0300

    Add full project

---
+++ js/script.js
@@ -0,0 +1,82 @@
+let pontos = 0
+
+function closeLogin(objeto){
+    let userName = document.body.querySelector('#nomeUsuario').value;
+    let code = document.body.querySelector('#codigo').value;
+    let form = document.querySelector('.form-container');
+
+    if( objeto.value === 'jogar' ) {
+        console.log('jogaaar')
+        if( userName.length === 0 || code.length === 0 ){
+            alert('Digite o nome de usuário e o código da sala para jogar')
+        } else {
+            form.parentNode.removeChild(form);
+            botaoInicial();
+            return;
+        }
+
+    }else if( objeto.value === 'criar_sala'){
+        console.log('criar sssala')
+        if( userName.length === 0){
+            alert('Digite seu nome de usuário para criar a sala')
+        } else {
+            form.parentNode.removeChild(form);
+            botaoInicial();
+            return;
+        }
+    }
+    
+    
+}
+
+function criarBalao() {
+    var balao = document.createElement('div');
+    balao.setAttribute("class",'balao');
+
+    //var x = Math.floor(Math.random()*window.innerWidth-50);
+    //var y = Math.floor(Math.random()*(window.innerHeight-50)) + 60 ;
+    var x = Math.floor(Math.random()*window.innerWidth-50);
+    var y = Math.floor(Math.random()*(window.innerHeight-50)) + 60 ;
+
+    balao.setAttribute('style','left:'+x+'px; top:'+y+'px');
+    balao.setAttribute('onclick','estourarBalao(this)');
+
+    document.body.appendChild(balao)
+}
+
+function estourarBalao(objeto) {
+    document.body.removeChild(objeto)
+
+    pontos++;
+    var score = document.getElementById('score')
+    score.innerHTML = `Balões estourados: ${pontos}`
+
+    document.body.appendChild(score)
+}
+
+function gerarAutomaticamenteBalao() {
+    
+    setInterval(criarBalao,250)
+    
+}
+
+
+function botaoInicial() {
+    var botao = document.createElement('button');
+    botao.setAttribute('class','botaodeIniciar');
+    botao.setAttribute('onclick','iniciarJogo(this)');
+    botao.innerHTML = "INICIAR";
+
+    document.body.appendChild(botao);
+}
+
+
+function iniciarJogo(objeto) {
+    document.body.removeChild(objeto)
+    gerarAutomaticamenteBalao()
+    
+}
+
+
+
+
